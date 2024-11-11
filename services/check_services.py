1c72049a22450eb663d1a0d1daee81ffd222b56d
Qano <andrian.qano@gmail.com>
2024-11-12 06:22:24 +0700

    modifikasi kode

--- services/check_services.py
+++ services/check_services.py
@@ -1,13 +1,19 @@
 import aiohttp
 from config import API_URL
+import json
+import subprocess
 
 async def check_nrk(nrk: str) -> dict:
     try:
         async with aiohttp.ClientSession() as session:
-            data = {"nrk": nrk} 
-            async with session.post(f"{API_URL}", json=data) as response:
+            payload = {"nrk": nrk} 
+            async with session.post(f"{API_URL}", data==payload) as response:
                 if response.status == 200:
-                    return await response.json() 
+                    formatted_response = json.loads(response.text)
+                    formatted_response_str = json.dumps(formatted_response, indent=2)
+                    jq_process = subprocess.Popen(['jq', '.'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
+                    jq_output, _ = jq_process.communicate(input=formatted_response_str)
+                    return await jq_output
                 else:
                     return None 
     except Exception:
