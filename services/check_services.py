3bb263ce8ddcfed91538df94ca2562525b578e3f
Qano <andrian.qano@gmail.com>
2024-11-12 07:08:54 +0700

    revisi

--- services/check_services.py
+++ services/check_services.py
@@ -1,7 +1,6 @@
 import aiohttp
 from config import API_URL
 import json
-import subprocess
 
 async def check_nrk(nrk: str) -> dict:
     try:
@@ -9,12 +8,15 @@ async def check_nrk(nrk: str) -> dict:
             payload = {"nrk": nrk} 
             async with session.post(f"{API_URL}", data=payload) as response:
                 if response.status == 200:
-                    formatted_response = json.loads(response.text)
-                    formatted_response_str = json.dumps(formatted_response, indent=2)
-                    jq_process = subprocess.Popen(['jq', '.'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
-                    jq_output, _ = jq_process.communicate(input=formatted_response_str)
-                    return await jq_output
+                    response_text = await response.text()
+                    try:
+                        formatted_response = json.loads(response_text)
+                        return formatted_response
+                    except json.JSONDecodeError:
+                        return None
                 else:
                     return None 
+    except aiohttp.ClientError:
+        return None
     except Exception:
-        return None 
+        return None
